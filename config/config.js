72f20acc1329f88b968ee24edb1c9b20b9c56e11
Ángel Gallego Zayas <xagalzay340@ieshnosmachado.org>
2025-02-21 10:16:08 +0100

    actualizacion

--- config/config.js
+++ config/config.js
@@ -1,7 +1,12 @@
 require("dotenv").config({
 path: `env.${process.env.NODE_ENV || "development"}`});
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
+
 module.exports = {
-  port: process.env.PORT || 80,
+  port: process.env.PORT || 8080,
   db: {
     host: process.env.DB_HOST || "http://switchyard.proxy.rlwy.net",
     user: process.env.DB_USER || "root",
