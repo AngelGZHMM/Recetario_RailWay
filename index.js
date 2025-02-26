cb77e34b0a87d309c331ba88303cd0b90efe57d0
Ángel Gallego Zayas <xagalzay340@ieshnosmachado.org>
2025-02-26 10:57:21 +0100

    actual

--- index.js
+++ index.js
@@ -1,53 +1,79 @@
-// index.js
+// Importar libreria para manejo de ficheros de configuración dependiendo de la variable de entorno NODE_ENV
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV || "development"}`,
+});
 
-// Importar la configuración (que ya carga las variables de entorno)
-const config = require('./config/config');
-
-// Importar librerías necesarias
+// Importar fichero de configuración con variables de entorno
+const config = require("./config/config");
+// Importar librería express --> web server
 const express = require("express");
+// Importar librería path, para manejar rutas de ficheros en el servidor
 const path = require("path");
+// Importar libreria CORS
 const cors = require("cors");
+// Importar librería de manejo de cookies
+const cookieParser = require("cookie-parser");
+// Importar gestores de rutas
+const platoRoutes = require("./routes/platoRoutes");
+const pedidoRoutes = require("./routes/pedidoRoutes");
+const userRoutes = require("./routes/userRoutes");
 
-// Importar rutas
-const pasosRoutes = require("./routes/pasosRoutes");
-const recetaRoutes = require("./routes/recetaRoutes");
-
-// Crear la instancia de Express
 const app = express();
-
-// Definir el puerto (se obtiene de la configuración)
-const port = config.port;
 
 // Configurar middleware para analizar JSON en las solicitudes
 app.use(express.json());
-console.log("Middleware JSON configurado");
 
-// Configurar CORS (descomenta la siguiente línea si lo requieres)
-// app.use(cors());
-console.log("CORS configurado");
+// Configurar CORS para admitir cualquier origen
+// app.use(cors()); // No permitite el envío de cookies en una API pública
+
+if (process.env.NODE_ENV !== "production") {
+  // Configurar CORS para admitir el origen del frontend en desarrollo
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // Permitir el frontend en desarrollo de React
+      //origin: "http://localhost:8081", // Permitir el frontend en desarrollo de React Native
+      credentials: true, // Permitir envío de cookies
+    })
+  );
+}
+
+// Habilitar el análisis de cookies
+app.use(cookieParser());
 
 // Configurar rutas de la API Rest
-app.use("/api/pasos", pasosRoutes);
-app.use("/api/receta", recetaRoutes);
-console.log("Rutas configuradas");
+app.use("/api/platos", platoRoutes);
+app.use("/api/pedidos", pedidoRoutes);
+app.use("/api/users", userRoutes);
 
-// Servir archivos estáticos desde la carpeta "public"
-app.use(express.static(path.join(__dirname, "public")));
-console.log("Servidor de archivos estáticos configurado");
+//Ruta para manejar las solicitudes al archivo index.html
 
-// Ruta base: cualquier otra ruta servirá el archivo index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
-console.log("Ruta base configurada");
+// app.get('/', (req, res) => {
+if (process.env.NODE_ENV !== "production") {
+  console.log("Sirviendo ficheros de desarrollo");
+  // Configurar el middleware para servir archivos estáticos desde el directorio public/dev en desarrollo
+  app.use(express.static(path.join(__dirname, "public/dev")));
 
-// Middleware para manejar errores
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/dev", "index.html"));
+  });
+} else {
+  console.log("Sirviendo ficheros de producción");
+  // Configurar el middleware para servir archivos estáticos desde el directorio public/dev en producción
+  app.use(express.static(path.join(__dirname, "public/prod")));
 
-// Iniciar el servidor (única llamada a app.listen)
-app.listen(port, () => {
-  console.log(`Servidor escuchando en el puerto ${port}`);
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/prod", "index.html"));
+  });
+}
+
+
+// Iniciar el servidor solo si no estamos en modo de prueba
+// en modo de prueba, el servidor se inicia en el archivo de prueba
+if (process.env.NODE_ENV !== "test") {
+  app.listen(config.port, () => {
+    console.log(`Servidor escuchando en el puerto ${config.port}`);
+  });
+}
+
+// Exportamos la aplicación para poder hacer pruebas
+module.exports = app;
